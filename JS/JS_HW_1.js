58bb1e7a0c5c2bb9adeb0e3c4968bfa323e629cd
gogvyatkin <vyatkinig@yandex.ru>
2022-04-23 09:22:22 +0300

    JS_HW_1 - DONE

--- JS/JS_HW_1.js
+++ JS/JS_HW_1.js
@@ -23,7 +23,23 @@ let item_7 = String(item_6);
 let item_7_type;
 item_7_type = typeof(item_7);
 console.log(typeof(item_7), "item_7 ==", item_7); console.log(typeof (item_7_type), "item_7_type ==", item_7_type);
-
-
-
-
+let age_1 = 10;
+let age_2 = 18;
+let age_3 = 60;
+if (age_1 == 10){
+    console.log("variable value is correct = ", age_1)
+} else {
+    console.log("invalid variable value age_1")
+}
+if (age_1 < age_2){
+    console.log('You don’t have access cause your age is ' + age_1 + ' It’s less then')
+}
+    else if(age_1 >= age_2 && age_1 < age_3){
+console.log('Welcome  !')
+    }
+    else if(age_1 > age_3){
+console.log("Keep calm and look Culture channel")
+    }
+    else{
+        console.log('Technical work')
+    }

658427f7c1b7fa7ad908cae44bd66fcdb3e2f981
gogvyatkin <vyatkinig@yandex.ru>
2022-05-01 07:44:32 +0300

    update HW_2

--- JS/JS_HW_2.js
+++ JS/JS_HW_2.js
@@ -1,14 +1,22 @@
 // Напишите валидационный скрипт используя функции 
 function script (trustr){
-    if (trustr.length < 5)
+    if (trustr.length < 5) //  3. Минимум 5 символов в строке
     console.log("less than 5 characters entered")
+else if (trustr.length > 64)
+console.log("etnter less characters")
+else if (/(@[A-Za-z])\w+/.test(trustr)) // Должна быть хотя бы одна @
+console.log("good")
+else if (trustr === "")
+console.log("NO GOOD!")
     else 
-    console.log("Everithing OK")
+    console.log("Everithing OK!")
 }
- script ("asdsd") // Скрипт должен на вход принимать строку.
-
-
-
+script ("adsdr") // Валидно!
+// script ("adsd") // Скрипт должен на вход принимать строку.
+ script ("adpo[aopsid[pasidasdpaosdkpaosfopjmvkamsopjasocjmasocaoscoancasncicnasocsd") // Максимум 64 символа в строке
+ script ("dsfj@sdkg")
+ script ("")
+//  /(@[A-Za-z])\w+/.test(str_text)
 
 
 //  2. После проверки строки выводить в консоль сообщение где будет конкретно написано, что не так в ведённой строке.

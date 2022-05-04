72e8145f1796170828e2f673d538b0e9cb401a0c
gogvyatkin <vyatkinig@yandex.ru>
2022-05-04 07:27:56 +0300

    last update HW_2

--- JS/JS_HW_2.js
+++ JS/JS_HW_2.js
@@ -1,45 +1,40 @@
 // Напишите валидационный скрипт используя функции 
 function script (trustr){
+// Строка не должна быть пустой    
     if (trustr === "")
-console.log("Enter any value!") // Строка не должна быть пустой
-
-else if (trustr.length < 5) //  3. Минимум 5 символов в строке
-console.log("less than 5 characters entered")
-
-else if (trustr.length > 64) // Максимум 64 символа в строке
-console.log("etnter less characters")
-
-else if (/[A-Za-z]/.test(trustr) == false)
-console.log("Enter word!")
-else if (/[0-9]/.test(trustr) == false)
-console.log("Enter any number!")
-else if (/@/.test(trustr) == false)
-console.log("Enter @!")
-else if (/[A-Z]/.test(trustr) == false)
-console.log("Enter an uppercase letter!")
+console.log("1. Enter some value!") 
+// Минимум 5 символов в строке
+else if (trustr.length < 5) 
+console.log("2. Enter more than 5 characters")
+// Максимум 64 символа в строке
+else if (trustr.length > 64) 
+console.log("3. Enter less 64 characters")
+// В строке должны быть буквы
+else if (/[A-Za-z]/.test(trustr) == false) 
+console.log("4. Enter a letter!")
+// Должна быть хотя бы одна цифра
+else if (/[0-9]/.test(trustr) == false) 
+console.log("5. Enter any number!")
+// Должна быть хотя бы одна @
+else if (/@/.test(trustr) == false) 
+console.log("6. Enter @")
+// Должна быть хотя бы одна буква в верхнем регистре
+else if (/[A-Z]/.test(trustr) == false) 
+console.log("7. Enter an uppercase letter!")
 else 
-console.log("Everithing OK!")
+console.log("8. Everithing OK!")
 }
 
+// Валидация полной формы Email
+// function script (trustr){    
+// if (/\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}/.test(trustr) == false)
+// console.log('Email invalid!')}
 
-script ("897246") // Валидно!
-script ("a@sd") // Скрипт должен на вход принимать строку.
-script ("adpo[aopsid[pasidasdpaosdk@aosfopjmvkamsopjasocjmasocaoscoancasncicnasocsd") // Максимум 64 символа в строке
-script ("") // Строка не должна быть пустой
-script ("wsfd1fg@df") // Должна быть хотя бы одна @
+script ("897246")
+script ("a@sd") 
+script ("adpo[aopsid[pasidasdpaosdk@aosfopjmvkamsopjasocjmasocaoscoancasncicnasocsd") 
+script ("") 
+script ("Wsfd1fg@df") 
 script ("Wsfdfgdf")
-
-
-//  /(@[A-Za-z])\w+/.test(str_text)
-// (\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}) Адрес email
-// else if (/(@[A-Za-z])\w+/.test(trustr) == false) // Должна быть хотя бы одна @
-// console.log("where is dog?")
-
-//  2. После проверки строки выводить в консоль сообщение где будет конкретно написано, что не так в ведённой строке.
-//  3. Минимум 5 символов в строке
-//  4. Максимум 64 символа в строке
-//  5. В строке должны быть буквы
-//  6. Должна быть хотя бы одна буква в верхнем регистре
-//  7. Должна быть хотя бы одна цифра
-//  8. Должна быть хотя бы одна @
-//  9. Строка не должна быть пустой
+script ("w34fd@fgd")
+script ("12WsWEWEWfgdf")

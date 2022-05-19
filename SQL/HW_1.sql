982d6023e37cd139b52fd421a437ae1af75c2a05
gogvyatkin <vyatkinig@yandex.ru>
2022-05-19 08:47:19 +0300

    update  HW_1.sql

--- SQL/HW_1.sql
+++ SQL/HW_1.sql
@@ -3,3 +3,19 @@ select "name", "id" from students;
 select "id" from students;
 select "name" from students;
 select "email" from students;
+select "name", "email" from students;
+select "id", "name", "email", "created_on" from students;
+select * from students
+where password= '12333';
+select * from students
+where created_on= '2021-03-26 00:00:00';
+select * from students
+where name like '%Anna%';
+select * from students
+where name like '%8';
+select * from students
+where name like '%a%';
+select * from students
+where created_on= '2021-07-12 00:00:00' and password like '1m313';
+
+

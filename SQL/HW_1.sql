f8e61075dd4871e168f6141198c5308f9f5928b9
gogvyatkin <vyatkinig@yandex.ru>
2022-05-21 09:38:17 +0300

    prelast update HW_1/sql

--- SQL/HW_1.sql
+++ SQL/HW_1.sql
@@ -1,5 +1,55 @@
-select * from students;
-select "name", "id" from students;
-select "id" from students;
+select * from students; -- 1
 select "name" from students;
+select "id" from students;
+select distinct "name" from students;
 select "email" from students;
+select "name", "email" from students;
+select "id", "name", "email", "created_on" from students;
+select * from students
+where password= '12333';
+select * from students
+where created_on= '2021-03-26 00:00:00';
+select * from students
+where name like '%Anna%';
+select * from students
+where name like '%8';
+select * from students
+where name like '%a%';
+select * from students
+where created_on= '2021-07-12 00:00:00';
+select * from students
+where created_on= '2021-07-12 00:00:00' and password like '1m313';
+select * from students
+where created_on= '2021-07-12 00:00:00' and name like '%Andrey%';
+select * from students
+where created_on= '2021-07-12 00:00:00' and name like '%8%';
+select * from students
+where id= 110;
+select * from students
+where id= 153;
+select * from students
+where id> 140;
+select * from students
+where id< 130;
+select * from students
+where id< 130 or id> 188;
+select * from students
+where id<= 137;
+select * from students
+where id>= 137;
+select * from students
+where id> 180 and id< 190;
+select * from students
+where password in ('12333', '1m313', '123313');
+select * from students
+where created_on in ('2020-10-03 00:00:00', '2021-05-19 00:00:00', '2021-03-26 00:00:00');
+select min(id) as MIN_id
+from students;
+select max(id) as MAX_id
+from students;
+select count(id) as TOTAL_students
+from students;
+select "id", "name", "created_on" from students
+order by created_on asc;
+select "id", "name", "created_on" from students
+order by created_on desc;

8d0b44a17d36d07e63190a15580c15c03bc4e0c2
gogvyatkin <vyatkinig@yandex.ru>
2022-05-22 22:42:19 +0300

    add file HW_2.1.sql

---
+++ SQL/HW_2.1.sql
@@ -0,0 +1,15 @@
+create table employees(
+id serial not null,
+employee_name varchar(50) not null,
+monthly_salary not null
+);
+select * from employees;
+
+
+--CREATE TABLE Persons (
+--    ID int NOT NULL,
+--    LastName varchar(255) NOT NULL,
+--    FirstName varchar(255) NOT NULL,
+--    Age int
+--);
+--select * from Persons;
